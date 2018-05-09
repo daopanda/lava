cb7fb01c8cf797c1c20b5372f1e68fd54354953b
daopanda <39012386+daopanda@users.noreply.github.com>
2018-05-09 11:43:37 +0300

    Add files via upload

--- build.js
+++ build.js
@@ -1,4 +1,7 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+
+
+
 var Vue = require('vue/dist/vue.js')
 
 var VueResource = require("vue-resource")
@@ -27,6 +30,8 @@ Vue.use(VueCookie)
 
 // Routing
 var router = new VueRouter({
+  history: true,
+  hashbang: false,
 	routes: [
 	{ 'path': '/singin', component: singin},
 	{ 'path': '/singup', component: singup},
